56fdef188dcb66ef530126a4c392fb3e7b47f77f
Ngoclann <junebride09.02@gmail.com>
2020-12-10 13:21:29 +0700

    Add new project

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using CoffeeShopManagement.dal;
+using CoffeeShopManagement.Entities;
+using CoffeeShopManagement.gui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeeShopManagement
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+              Application.EnableVisualStyles();
+               Application.SetCompatibleTextRenderingDefault(false);
+               Application.Run(new Login());  
+          //  StaffDao staffDao = new StaffDao();
+         //   Staff staff = new Staff();
+        }
+    }
+}
