f79e58097855e379c873984e2133ed86a53df107
Ngoclann <junebride09.02@gmail.com>
2020-12-10 13:27:40 +0700

    Add dal

---
+++ dal/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CoffeeShopManagement.dal
+{
+    class DBContext
+    {
+        public static SqlConnection GetConnection()
+        {
+            string connString =  ConfigurationManager.ConnectionStrings["connect"].ConnectionString;
+            //string connString = @"Data Source=" + datasource + ";Initial Catalog="
+            //        + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password;
+            SqlConnection conn = new SqlConnection(connString);
+            return conn;
+        }
+    }
+}
---
+++ dal/UserDao.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopManagement.dal
+{
+    class UserDao
+    {
+        public bool isExisted(string username, string password)
+        {
+            bool isExisted = false;
+            SqlConnection conn = DBContext.GetConnection();
+            conn.Open();
+            String sql = "select * from users where username = '" + username + "'" + "and password = '" + password + "'";
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            da.Fill(ds, "users");
+            if (ds.Tables["users"].Rows.Count > 0) isExisted = true;
+            conn.Close();  
+            return isExisted;
+        }
+
+
+        public string getPasswordByUsername(String username)
+        {
+            SqlConnection conn = DBContext.GetConnection();
+            conn.Open();
+            String sql = "select password from users where username = '" + username + "'";
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            da.Fill(ds, "users");
+            if (ds.Tables["users"].Rows.Count > 0) return ds.Tables["users"].Rows[0].ItemArray[0].ToString();
+            return "";
+        }
+
+        public void changePassword(string username, string password)
+        {
+            SqlConnection conn = DBContext.GetConnection();
+            conn.Open();
+            String sql = "update users set password = @password where username = @username";
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = sql;
+            cmd.Parameters.Add("@username", SqlDbType.NVarChar).Value = username;
+            cmd.Parameters.Add("@password", SqlDbType.NVarChar).Value = password;
+            cmd.ExecuteNonQuery();
+        }
+
+        public bool isExistIUsername(string username)
+        {
+            bool isExisted = false;
+            SqlConnection conn = DBContext.GetConnection();
+            conn.Open();
+            String sql = "select * from users where username = '" + username + "'";
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            da.Fill(ds, "users");
+            if (ds.Tables["users"].Rows.Count > 0) isExisted = true;
+            conn.Close();
+            return isExisted;
+        }
+
+        public void insert(string username, string password)
+        {
+            SqlConnection conn = null;
+            String sql = "insert into users(username, password) values(@username, @password)";
+            conn = DBContext.GetConnection();
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = sql;
+                cmd.Parameters.Add("@username", SqlDbType.NVarChar).Value = username;
+                cmd.Parameters.Add("@password", SqlDbType.NVarChar).Value = password;
+                cmd.ExecuteNonQuery();
+           
+        }
+    }
+}
